06b4cdfb756d59fb2e4059ad5e4826b1da07c868
Patrick BIYAGA <biyagapatrick@gmail.com>
2020-08-03 22:49:51 +0200

    Update Modules, Create successfuly user

--- Module2/models/user.js
+++ Module2/models/user.js
@@ -24,13 +24,13 @@ const User = mongoose.model('User', new mongoose.Schema({
 }));
 
 function validateUser(user) {
-	const schema = {
+	const schema = Joi.object({
 		name: Joi.string().min(5).max(50).required(),
 		email: Joi.string().min(5).max(255).required().email(),
 		password: Joi.string().min(5).max(255).required()
-	};
+	});
 
-	return Joi.validate(user, schema);
+	return Joi.assert(user, schema);
 }
 
 
--- Module2/routes/users.js
+++ Module2/routes/users.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-	const { error } = validate(req.body);
+	const error = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
 	let user = await User.findOne({ email: req.body.email });
